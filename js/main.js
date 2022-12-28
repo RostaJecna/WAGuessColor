0e83ace3272eae3bf1d8abcb0a8e0eb3871f0f8b
RostaJecna <levder@spsejecna.cz>
2022-12-28 17:53:33 +0100

    Add method for generating circles

--- js/main.js
+++ js/main.js
@@ -22,6 +22,28 @@ function generateHex() {
     return Math.floor(Math.random() * 16777215).toString(16);
 }
 
+function generateCircles(count = difficultyValue.innerHTML) {
+    // Deletes all circles that were in the list
+    listOfCircles.innerHTML = '';
+
+    for (let i = 0; i < count; i++) {
+        let randomHex = generateHex();
+
+        while (randomHex.length < 6) randomHex = generateHex();
+        const randomColor = '#' + randomHex;
+
+        const circle = document.createElement("li");
+        circle.classList.add("rounded-circle");
+        circle.style.backgroundColor = randomColor;
+        circle.id = randomColor;
+
+        listOfCircles.appendChild(circle);
+    }
+
+    let randomCircle = listOfCircles.getElementsByTagName("li").item(Math.floor(Math.random() * count));
+    hexCodeText.textContent = correctCircleID = randomCircle.id;
+}
+
 function hideFooter() {
     footer.classList.remove("d-flex");
     newGameBtn.classList.remove("d-grid");
